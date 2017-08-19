00e03a7f8350b6cf9fc91893e7ce3ceaba0b8018
CodFrm <code.farmer@qq.com>
2017-08-19 19:34:14 +0800

    添加手势,音量,亮度调节,添加时间,优化全屏显示

--- app/src/main/java/com/example/codef/ijkplay_demo/ijkplayer/ijkvideo.java
+++ app/src/main/java/com/example/codef/ijkplay_demo/ijkplayer/ijkvideo.java
@@ -2,12 +2,15 @@ package com.example.codef.ijkplay_demo.ijkplayer;
 
 import android.app.Activity;
 import android.app.Instrumentation;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Resources;
+import android.media.AudioManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -16,10 +19,12 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -41,6 +46,14 @@ public class ijkvideo {
     private ImageButton fullButton;
     private TextView mTitleText;
     private ImageButton playBack;
+    private LinearLayout mTimeCtrl;
+    private TextView playTime;
+    private TextView playAllTime;
+    private myGestureListener mGesture;
+    private RelativeLayout mViewLight;
+    private RelativeLayout mViewSound;
+    private ProgressBar barLight;
+    private ProgressBar barSound;
 
     public ijkvideo(Context context) {
         mContext = context;
@@ -72,11 +85,22 @@ public class ijkvideo {
     public GestureDetector mGestureDetector;
 
     public void createPlayer() {
+        isCreate = true;
         LayoutInflater inflater = LayoutInflater.from(mContext);
         mViewHolder = (RelativeLayout) inflater.inflate(R.layout.play, null).findViewById(R.id.combineCtrl);
         mVideoView = (IjkVideoView) mViewHolder.findViewById(R.id.video_view);
         mPlayTop = (LinearLayout) mViewHolder.findViewById(R.id.play_top);
         mPlayController = (LinearLayout) mViewHolder.findViewById(R.id.play_controller);
+        mTimeCtrl = (LinearLayout) mViewHolder.findViewById(R.id.time_ctrl);
+        mViewLight = (RelativeLayout) mViewHolder.findViewById(R.id.view_light);
+        mViewLight.setVisibility(View.GONE);
+        barLight = (ProgressBar) mViewHolder.findViewById(R.id.bar_light);
+        mViewSound = (RelativeLayout) mViewHolder.findViewById(R.id.view_sound);
+        mViewSound.setVisibility(View.GONE);
+        barSound = (ProgressBar) mViewHolder.findViewById(R.id.bar_sound);
+        mTimeCtrl.setVisibility(View.GONE);
+        playTime = (TextView) mViewHolder.findViewById(R.id.now_time);
+        playAllTime = (TextView) mViewHolder.findViewById(R.id.all_time);
         mTitleText = (TextView) mViewHolder.findViewById(R.id.play_title);
         RelativeLayout.LayoutParams rllp = new RelativeLayout.LayoutParams(mWidth, mHeight);
         rllp.leftMargin = mLeft;
@@ -84,10 +108,14 @@ public class ijkvideo {
         ((Activity) mContext).addContentView(mViewHolder, rllp);
         hidden();
         toggleAspectRatio(1);
-        mGestureDetector = new GestureDetector(mViewHolder.getContext(), new myGestureListener());
+        mGesture = new myGestureListener();
+        mGestureDetector = new GestureDetector(mViewHolder.getContext(), mGesture);
         mViewHolder.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == 1 && mGesture.isScroll) {//GestureDetector的onScroll竟然不会触发手指抬起事件,只好自己来实现了
+                    return mGesture.onSingleTapUp(motionEvent);
+                }
                 return mGestureDetector.onTouchEvent(motionEvent);
             }
         });
@@ -114,12 +142,12 @@ public class ijkvideo {
                 }
             }
         });
-        playBack=(ImageButton)mViewHolder.findViewById(R.id.play_back);
+        playBack = (ImageButton) mViewHolder.findViewById(R.id.play_back);
         playBack.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                if(isFull){
+                if (isFull) {
                     fullScreen();
-                }else{
+                } else {
                     Instrumentation inst = new Instrumentation();
                     inst.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK);
                 }
@@ -134,18 +162,19 @@ public class ijkvideo {
             }
         });
         seekBar = (SeekBar) mViewHolder.findViewById(R.id.play_seekbar);
-        seekBar.setMax(100);
+        seekBar.setMax(10000);//为了拖动的精确,设置最大为1w
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 isClick = false;
-                int value = (int) Math.floor((double) allTime * ((double) seekBar.getProgress() / 100));
+                int value = (int) Math.floor((double) allTime * ((double) seekBar.getProgress() / 10000));
                 mVideoView.seekTo(value);
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
                 isClick = true;
+                delay = 5000;
             }
 
             @Override
@@ -159,6 +188,7 @@ public class ijkvideo {
                 if (i == IMediaPlayer.MEDIA_INFO_AUDIO_RENDERING_START) {
                     allTime = mVideoView.getDuration();
                     Timer timer = new Timer();
+                    playAllTime.setText("/" + timeLengthToTime(allTime));
                     timer.schedule(new TimerTask() {
                         public void run() {
                             if (!isCreate) {
@@ -182,7 +212,37 @@ public class ijkvideo {
                 }
             }
         });
-        isCreate = true;
+        barLight.setProgress(getLight());
+        AudioManager am = (AudioManager) ((Activity) mContext).getSystemService(Context.AUDIO_SERVICE);
+        barSound.setMax(am.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        barSound.setProgress(am.getStreamVolume(AudioManager.STREAM_MUSIC));
+    }
+
+    private int getLight() {
+        Window window = ((Activity) mContext).getWindow();
+        WindowManager.LayoutParams lp = window.getAttributes();
+        if (lp.screenBrightness == -1) {
+            return getScreenBrightness();
+        }
+        return (int) Math.floor(lp.screenBrightness * 255f);
+    }
+
+    public void setLight(int brightness) {
+        Window window = ((Activity) mContext).getWindow();
+        WindowManager.LayoutParams lp = window.getAttributes();
+        if (brightness == -1) {
+            lp.screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE;
+        } else {
+            lp.screenBrightness = (brightness <= 0 ? 1 : brightness) / 255f;
+        }
+        window.setAttributes(lp);
+    }
+
+    private int getScreenBrightness() {
+        ContentResolver contentResolver = ((Activity) mContext).getContentResolver();
+        int defVal = 125;
+        return Settings.System.getInt(contentResolver,
+                Settings.System.SCREEN_BRIGHTNESS, defVal);
     }
 
     private boolean isCreate = false;
@@ -213,6 +273,7 @@ public class ijkvideo {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 fullButton.setImageDrawable(mVideoView.getResources().getDrawable(R.drawable.btn_full, null));
             }
+            mTimeCtrl.setVisibility(View.GONE);
             isFull = false;
         } else {
             WindowManager.LayoutParams params = ((Activity) mContext).getWindow().getAttributes();
@@ -231,19 +292,23 @@ public class ijkvideo {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 fullButton.setImageDrawable(mVideoView.getResources().getDrawable(R.drawable.btn_mini, null));
             }
+            mTimeCtrl.setVisibility(View.VISIBLE);
             isFull = true;
         }
         show();
     }
+
     //隐藏虚拟按键
-    private void hiddenNavigation(){
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY ;
+    private void hiddenNavigation() {
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         ((Activity) mContext).getWindow().getDecorView().setSystemUiVisibility(uiOptions);
     }
-    private void showNavigation(){
-        int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE  ;
+
+    private void showNavigation() {
+        int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
         ((Activity) mContext).getWindow().getDecorView().setSystemUiVisibility(uiOptions);
     }
+
     public void move(int left, int top, int width, int height) {
         ViewGroup.LayoutParams rllp = mViewHolder.getLayoutParams();
         rllp.width = width;
@@ -275,9 +340,10 @@ public class ijkvideo {
             switch (msg.arg1) {
                 case IMediaPlayer.MEDIA_INFO_AUDIO_RENDERING_START: {
                     int nowTime = mVideoView.getCurrentPosition();
-//                    playTime.setText(timeLengthToTime(nowTime));
-                    if (!isClick) {
-                        int value = (int) Math.floor(((double) nowTime / (double) allTime) * 100);
+                    seekBar.setSecondaryProgress(mVideoView.getBufferPercentage() * 100);
+                    if (!mGesture.isScroll && mGesture.Touch != myGestureListener.TOUCH_X) {
+                        playTime.setText(timeLengthToTime(nowTime));
+                        int value = (int) Math.floor(((double) nowTime / (double) allTime) * 10000);
                         seekBar.setProgress(value);
                     }
                     break;
@@ -314,15 +380,129 @@ public class ijkvideo {
             super();
         }
 
+        private boolean isScroll = false;
+        private int nowTime = 0;
+
         @Override
-        public boolean onDown(MotionEvent event) {
+        public boolean onDown(MotionEvent event) {//手指按下
+            if (isShowing()) {
+                isClick = true;
+                delay = 5000;
+            }
             return true;
         }
 
+        public static final int TOUCH_NULL = -1;//啥都没
+        public static final int TOUCH_X = 0;//手指横向滑动
+        public static final int TOUCH_LEFT_Y = 1;//手指左边纵向滑动
+        public static final int TOUCH_RIGHT_Y = 2;//手指右边纵向滑动
+        public int Touch = TOUCH_NULL;
+        private int Progress = 0;//记录初始进度
+        private int delay=0;
+        private void sleepHide (final View v) {
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    if (delay > 0) {
+                        if (Touch!=TOUCH_NULL) {
+                            delay = 2000;
+                        } else {
+                            delay -= 1000;
+                        }
+                        sleepHide(v);
+                    } else {
+                        v.setVisibility(View.GONE);
+                    }
+                }
+            }, 1000);
+        }
         @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2,      //滑动事件
-                               float velocityX, float velocityY) {
-            return false;
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {//手指在触摸屏上滑动
+            if (Touch == TOUCH_NULL) {
+                Touch = getTouch(e1.getX(), e1.getY(), e2.getX(), e2.getY());
+                isClick = true;
+                isScroll = true;
+                if (Touch == TOUCH_X) {
+                    Progress = seekBar.getProgress();
+                    nowTime = mVideoView.getCurrentPosition();
+                } else if (Touch == TOUCH_LEFT_Y) {
+                    Progress = barLight.getProgress();
+                    mViewLight.setVisibility(View.VISIBLE);
+                    delay=2000;
+                    sleepHide(mViewLight);
+                } else if (Touch == TOUCH_RIGHT_Y) {
+                    AudioManager am = (AudioManager) ((Activity) mContext).getSystemService(Context.AUDIO_SERVICE);
+                    barSound.setProgress(am.getStreamVolume(AudioManager.STREAM_MUSIC));
+                    Progress = barSound.getProgress();
+                    delay=2000;
+                    sleepHide(mViewSound);
+                }
+            } else if (Touch == TOUCH_X) {
+                //从左到右滚一次90秒
+                ViewGroup.LayoutParams rllp = mViewHolder.getLayoutParams();
+                float time = (90 / (float) getWidth()) * (e1.getX() - e2.getX());
+                int x = (int) Math.floor(time * (10000 / (allTime / 1000)));
+                seekBar.setProgress((int) (Progress - (int) (x)));
+                playTime.setText(timeLengthToTime((int) Math.floor((float) nowTime - time * 1000)));//计算时间
+            } else if (Touch == TOUCH_LEFT_Y) {
+                float x = (127.5f / (float) getHeight()) * (e1.getY() - e2.getY());
+                barLight.setProgress((int) (Progress + (int) Math.floor(x)));
+                setLight(Progress + (int) Math.floor(x));
+            } else if (Touch == TOUCH_RIGHT_Y) {
+                AudioManager am = (AudioManager) ((Activity) mContext).getSystemService(Context.AUDIO_SERVICE);
+                float x = (am.getStreamMaxVolume(AudioManager.STREAM_MUSIC) / 2 / (float) getHeight()) * (e1.getY() - e2.getY());
+                barSound.setProgress((int) (Progress + (int) Math.floor(x)));
+                am.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (Progress + (int) Math.floor(x)), AudioManager.FLAG_PLAY_SOUND);
+            }
+            return true;
+        }
+
+        private int getWidth() {
+            ViewGroup.LayoutParams rllp = mViewHolder.getLayoutParams();
+            if (rllp.width == -1) {
+                Resources resources = mViewHolder.getResources();
+                DisplayMetrics dm = resources.getDisplayMetrics();
+                return dm.widthPixels;
+            }
+            return rllp.width;
+        }
+
+        private int getHeight() {
+            ViewGroup.LayoutParams rllp = mViewHolder.getLayoutParams();
+            if (rllp.height == -1) {
+                Resources resources = mViewHolder.getResources();
+                DisplayMetrics dm = resources.getDisplayMetrics();
+                return dm.heightPixels;
+            }
+            return rllp.height;
+        }
+
+        private int getTouch(float x, float y, float lx, float ly) {
+            if (Math.abs(y - ly) < 20) {//判断y,绝对值小于20则认为是横向滑动
+                return TOUCH_X;
+            } else if (Math.abs(y - ly) > 20) {
+                Resources resources = mContext.getResources();
+                DisplayMetrics dm = resources.getDisplayMetrics();
+                if ((dm.widthPixels / 2) > x) {
+                    return TOUCH_LEFT_Y;
+                }
+                return TOUCH_RIGHT_Y;
+            }
+            return TOUCH_NULL;
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {//手指抬起
+            Log.e(TAG, "手指抬起");
+            isClick = false;
+            isScroll = false;
+            if (Touch != TOUCH_NULL) {
+                if (Touch == TOUCH_X) {
+                    int value = (int) Math.floor((double) allTime * ((double) seekBar.getProgress() / 10000));
+                    mVideoView.seekTo(value);
+                }
+            }
+            Touch = TOUCH_NULL;
+            return true;
         }
 
         @Override
@@ -369,7 +549,7 @@ public class ijkvideo {
         mPlayTop.setVisibility(View.INVISIBLE);
         mPlayController.setVisibility(View.INVISIBLE);
         //全屏隐藏虚拟按键
-        if(isFull){
+        if (isFull) {
             hiddenNavigation();
         }
     }
@@ -382,7 +562,7 @@ public class ijkvideo {
     }
 
     public void show(int time) {
-        if (isFull){
+        if (isFull) {
             showNavigation();
         }
         mPlayTop.setVisibility(View.VISIBLE);
